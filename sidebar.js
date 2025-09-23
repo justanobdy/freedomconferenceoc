a1903905986799c4477308b031a1cd413426f02a
justanobdy <56369473+justanobdy@users.noreply.github.com>
2025-09-23 12:35:41 -0700

    starting stuff

---
+++ sidebar.js
@@ -0,0 +1,7 @@
+function enableSidebar() {
+    document.getElementById("nav").style.display = "block";
+}
+
+function disableSidebar() {
+    document.getElementById("nav").style.display = "none";
+}
