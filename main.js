65bae7416246374e368dae4795616d7dd2cb19c5
justanobdy <56369473+justanobdy@users.noreply.github.com>
2025-09-23 20:51:30 -0700

    more stuff

---
+++ main.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function(event) {
+    document.getElementById("topbar").innerHTML = `
+            <a type="button" class="topbar-text btn" href="index.html">Home</a>
+            <a type="button" class="topbar-text btn" href="about.html">About</a>
+            <a type="button" class="topbar-text btn" href="schedule.html">Schedule</a>
+            <a type="button" class="topbar-text btn" href="media.html">Media</a>
+        `
+});
